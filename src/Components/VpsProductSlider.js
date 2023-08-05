7161fb4c89aec30daa977cafdbdd7f1a8b6f2fcc
sheetaldhingra <sheetaldhingra4@gmail.com>
2023-08-05 09:02:40 +0530

    product changes

--- src/Components/VpsProductSlider.js
+++ src/Components/VpsProductSlider.js
@@ -4,6 +4,8 @@ import ReactCardSlider from "react-card-slider-component";
 import $ from "jquery";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 export default function VpsProductSlider() {
   const [slideData, setSlideData] = useState([]);
   const navigate = useNavigate();
@@ -369,6 +371,30 @@ export default function VpsProductSlider() {
   useEffect(() => {
     getProductsData();
   }, [search]);
+  const breakpoints = {
+    768: {
+      perPage: 1, // Show 1 slide per page on screens with width <= 768px (phones)
+    },
+    992: {
+      perPage: 3, // Show 3 slides per page on screens with width <= 992px (tablets)
+    },
+    1200: {
+      perPage: 4, // Show 4 slides per page on screens with width > 992px (desktops)
+    },
+  };
+  function sliderClick1(index) {
+    if(index != undefined && slideClick.length > 0) {
+    navigate("/singleProduct", {
+      state: {
+        name: slideClick[index].title,
+        description: slideClick[index].description,
+        image: slideClick[index].image,
+      },
+    });
+    $("#navbarTogglerDemo02").find(".nav-link").removeClass("active");
+    $("#navbarTogglerDemo02").find(".products").addClass("active");
+  }
+  };
   return (
     <>
       <h1
@@ -397,12 +423,37 @@ export default function VpsProductSlider() {
           <span className="visually-hidden">Loading...</span>
         </div>
       </div>
-      <div
-        className="mt-4 d-flex justify-content-center align-items-center moldContent"
-        style={{ display: "none" }}
-      >
-        <ReactCardSlider className="sdd" slides={slideData} />
+      <div className="container">
+      <div className="row">
+        <div className="col">
+          <Splide
+            options={{
+              rewind: true,
+              perPage: 4,
+              perMove: 1,
+              focus: 0,
+              omitEnd: true,
+              wheel: false,
+              pagination: false,
+              breakpoints: breakpoints,
+            }}
+            aria-label="React Splide Example"
+          >
+            {slideData.map((slide, index) => (
+              <SplideSlide key={index} className="splide__slide" slide_id={index} onClick={sliderClick1(index)}>
+                <div onClick={sliderClick1(index)}>
+                  <img src={slide.image} alt={`Slide ${index}`} style={{ width: '100%', height: 'auto' }} />
+                  <div className="slide-content">
+                    <h3>{slide.title}</h3>
+                    <p>{slide.description}</p>
+                  </div>
+                </div>
+              </SplideSlide>
+            ))}
+          </Splide>
+        </div>
       </div>
+    </div>
       <div className="container d-flex justify-content-center my-5">
         <Link className="btn btn-primary text-light" to="/products">
           READ MORE ABOUT MOLDS
