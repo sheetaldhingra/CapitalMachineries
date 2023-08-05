7cc1025ca6acf137d15dc62e05010b74c8722ecb
sheetaldhingra <sheetaldhingra4@gmail.com>
2023-08-05 09:08:19 +0530

    product changes

--- src/Components/VpsProductSlider.js
+++ src/Components/VpsProductSlider.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import ReactCardSlider from "react-card-slider-component";
 import $ from "jquery";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
