9ed9fcd500e2cc26e32552cbcd16858ce81543cd
sheetaldhingra <sheetaldhingra4@gmail.com>
2023-12-12 02:12:21 +0530

    logos updated

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-import ContactUsForm from './ContactUsForm';
+// import ContactUsForm from './ContactUsForm';
 import Vpsheader from './Vpsheader';
 import VpsFooter from './VpsFooter';
 export default function ContactUs() {
@@ -20,7 +20,7 @@ export default function ContactUs() {
         {/* <h2 className='m-3 d-flex justify-content-center'>Feel Free to Contact Us</h2> */}
         <iframe title='capitalmachinery' className='mt-4' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3507.310053141891!2d77.02016757502777!3d28.47020779143431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d19d2177cbbc1%3A0x14edcf928038fb6e!2sCapital%20Machineries!5e0!3m2!1sen!2sin!4v1690702319378!5m2!1sen!2sin" width="100%" height="450" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
 <div className=''>
-<ContactUsForm/>
+{/* <ContactUsForm/> */}
 </div>
 <hr/>
     </Wrapper>
--- src/Components/VpsProductSlider.js
+++ src/Components/VpsProductSlider.js
@@ -25,370 +25,64 @@ export default function VpsProductSlider() {
 
   const search = "";
   const getProductsData = async () => {
-    $(".moldLoader").addClass("d-flex").removeClass("d-none");
-    $(".moldContent").removeClass("d-flex").addClass("d-none");
+    //$(".moldLoader").addClass("d-flex").removeClass("d-none");
+    //$(".moldContent").removeClass("d-flex").addClass("d-none");
     slides = [];
-    // const res = await fetch(process.env.REACT_APP_LocalUrl + "/products?isShow=1&page=1&limit=4", {
-    const Boschres = await fetch(
-      process.env.REACT_APP_VercelUrl +
-        "/bosch?isShow=1&page=1&limit=4&page=1&limit=4",
-      {
-        method: "GET",
-        headers: headers,
-      }
-    )
-      .then((response) => {
-        if (!response.ok) {
-        }
-        return response.json();
-      })
-      .then((data) => {
-        $(data.myData).each(function (pr, prod) {
-          if (
-            (prod.IsShow || prod.IsShow === "true") &&
-            (prod.Active || prod.Active === "true")
-          ) {
-            slides.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.ShortDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            slideClick.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.LongDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            //setSlideData(slides);
-          }
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    const forteres = await fetch(
-      process.env.REACT_APP_VercelUrl + "/forte?isShow=1&page=1&limit=4",
-      {
-        method: "GET",
-        headers: headers,
-      }
-    )
-      .then((response) => {
-        if (!response.ok) {
-        }
-        return response.json();
-      })
-      .then((data) => {
-        $(data.myData).each(function (pr, prod) {
-          if (
-            (prod.IsShow || prod.IsShow === "true") &&
-            (prod.Active || prod.Active === "true")
-          ) {
-            slides.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.ShortDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            slideClick.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.LongDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            //setSlideData(slides);
-          }
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    const sandhures = await fetch(
-      process.env.REACT_APP_VercelUrl + "/sandhu?isShow=1&page=1&limit=4",
-      {
-        method: "GET",
-        headers: headers,
-      }
-    )
-      .then((response) => {
-        if (!response.ok) {
-        }
-        return response.json();
-      })
-      .then((data) => {
-        $(data.myData).each(function (pr, prod) {
-          if (
-            (prod.IsShow || prod.IsShow === "true") &&
-            (prod.Active || prod.Active === "true")
-          ) {
-            slides.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.ShortDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            slideClick.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.LongDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            //setSlideData(slides);
-          }
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    const durares = await fetch(
-      process.env.REACT_APP_VercelUrl + "/dura?isShow=1&page=1&limit=4",
-      {
-        method: "GET",
-        headers: headers,
-      }
-    )
-      .then((response) => {
-        if (!response.ok) {
-        }
-        return response.json();
-      })
-      .then((data) => {
-        $(data.myData).each(function (pr, prod) {
-          if (
-            (prod.IsShow || prod.IsShow === "true") &&
-            (prod.Active || prod.Active === "true")
-          ) {
-            slides.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.ShortDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            slideClick.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.LongDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            //setSlideData(slides);
-          }
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    const heaprores = await fetch(
-      process.env.REACT_APP_VercelUrl + "/heapro?isShow=1&page=1&limit=4",
-      {
-        method: "GET",
-        headers: headers,
-      }
-    )
-      .then((response) => {
-        if (!response.ok) {
-        }
-        return response.json();
-      })
-      .then((data) => {
-        $(data.myData).each(function (pr, prod) {
-          if (
-            (prod.IsShow || prod.IsShow === "true") &&
-            (prod.Active || prod.Active === "true")
-          ) {
-            slides.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.ShortDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            slideClick.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.LongDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            //setSlideData(slides);
-          }
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    const starres = await fetch(
-      process.env.REACT_APP_VercelUrl + "/star?isShow=1&page=1&limit=4",
-      {
-        method: "GET",
-        headers: headers,
-      }
-    )
-      .then((response) => {
-        if (!response.ok) {
-        }
-        return response.json();
-      })
-      .then((data) => {
-        $(data.myData).each(function (pr, prod) {
-          if (
-            (prod.IsShow || prod.IsShow === "true") &&
-            (prod.Active || prod.Active === "true")
-          ) {
-            slides.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.ShortDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            slideClick.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.LongDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            //setSlideData(slides);
-          }
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    const rallires = await fetch(
-      process.env.REACT_APP_VercelUrl + "/ralli?isShow=1&page=1&limit=4",
-      {
-        method: "GET",
-        headers: headers,
-      }
-    )
-      .then((response) => {
-        if (!response.ok) {
-        }
-        return response.json();
-      })
-      .then((data) => {
-        $(data.myData).each(function (pr, prod) {
-          if (
-            (prod.IsShow || prod.IsShow === "true") &&
-            (prod.Active || prod.Active === "true")
-          ) {
-            slides.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.ShortDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            slideClick.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.LongDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            //setSlideData(slides);
-          }
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    const warppres = await fetch(
-      process.env.REACT_APP_VercelUrl + "/warpp?isShow=1&page=1&limit=4",
-      {
-        method: "GET",
-        headers: headers,
-      }
-    )
-      .then((response) => {
-        if (!response.ok) {
-        }
-        return response.json();
-      })
-      .then((data) => {
-        $(data.myData).each(function (pr, prod) {
-          if (
-            (prod.IsShow || prod.IsShow === "true") &&
-            (prod.Active || prod.Active === "true")
-          ) {
-            slides.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.ShortDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            slideClick.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.LongDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            //setSlideData(slides);
-          }
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    const res = await fetch(
-      process.env.REACT_APP_VercelUrl + "/products?isShow=1&page=1&limit=4",
-      {
-        method: "GET",
-        headers: headers,
-      }
-    )
-      .then((response) => {
-        if (!response.ok) {
-        }
-        $(".moldLoader").removeClass("d-flex").addClass("d-none");
-        $(".moldContent").addClass("d-flex").removeClass("d-none");
-        return response.json();
-      })
-      .then((data) => {
-        $(".moldLoader").removeClass("d-flex").addClass("d-none");
-        $(".moldContent").addClass("d-flex").removeClass("d-none");
-        $(data.myData).each(function (pr, prod) {
-          if (
-            (prod.IsShow || prod.IsShow === "true") &&
-            (prod.Active || prod.Active === "true")
-          ) {
-            slides.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.ShortDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            slideClick.push({
-              image: prod.image,
-              title: prod.title,
-              description: prod.LongDescription,
-              LongDescription: prod.LongDescription,
-              clickEvent: () => sliderClick(pr),
-            });
-            slides = slides.filter(
-              (arr, index, self) =>
-                index === self.findIndex((t) => t.title === arr.title)
-            );
-            setSlideData(slides);
-          }
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+
+    slides.push({
+      image: '/Bosch/BoschLogo.webp',
+      title: 'Bosch',
+      description: 'Bosch',
+      LongDescription: 'Bosch',
+      url:'/products/bosch'
+    });
+    slides.push({
+      image: '/Forte/fortelogoo.webp',
+      title: 'Forte',
+      description: 'Forte',
+      LongDescription: 'Forte',
+      url:'/products/forte'
+    });
+    slides.push({
+      image: '/Sandhu/sandhulogo.png',
+      title: 'Sandhu',
+      description: 'Sandhu',
+      LongDescription: 'Sandhu',
+      url:'/products/sandhu'
+    });
+    slides.push({
+      image: '/great_yuva/greatlogo.png',
+      title: 'Great Yuva',
+      description: 'Great Yuva',
+      LongDescription: 'Great Yuva',
+      url:'/products/greatyuva'
+    });
+    slides.push({
+      image: '/RalliWolf/ralliwolf-logo.png',
+      title: 'Ralli Wolf',
+      description: 'Ralli Wolf',
+      LongDescription: 'Ralli Wolf',
+      url:'/products/ralli',
+      color:'#df0202'
+    });
+    slides.push({
+      image: '/StarBlaze/starblazelogo.svg',
+      title: 'Star Blaze',
+      description: 'Star Blaze',
+      LongDescription: 'Star Blaze',
+      url:'/products/starblaze'
+    });
+    slides.push({
+      image: '/Warpp/warpplogo.webp',
+      title: 'Warpp',
+      description: 'Warpp',
+      LongDescription: 'Warpp',
+      url:'/products/warpp'
+    });
+    setSlideData(slides);
+
+    $(".moldLoader").addClass("d-none").removeClass("d-none");
+    $(".moldContent").removeClass("d-none").addClass("d-flex");
   };
   useEffect(() => {
     getProductsData();
@@ -425,26 +119,6 @@ export default function VpsProductSlider() {
       >
         We Deal In...
       </h1>
-      <div className="container d-flex justify-content-center my-5 moldLoader">
-        <div className="spinner-grow spinner-grow-sm mx-1" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-grow spinner-grow-sm mx-1" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-grow spinner-grow-sm mx-1" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-grow spinner-grow-sm mx-1" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-grow spinner-grow-sm mx-1" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-grow spinner-grow-sm mx-1" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-      </div>
       <div className="container">
         <div className="row">
           <div className="col">
@@ -464,20 +138,21 @@ export default function VpsProductSlider() {
               {slideData.map((slide, index) => (
                 <SplideSlide
                   key={index}
-                  className="splide__slide"
+                  className="splide__slide px-1"
                   slide_id={index}
                   onClick={sliderClick1(index)}
                 >
-                  <Link to="/singleProduct" state={{name: `${slide.title}`, image: `${slide.LongDescription.split(",")[0]}`, description: `${slide.description}`}}>
+                  <Link to={slide.url} target="_blank" state={{name: `${slide.title}`, image: `${slide.LongDescription.split(",")[0]}`, description: `${slide.description}`}}>
                   <div onClick={sliderClick1(index)}>
                     <img
-                      src={slide.LongDescription.split(",")[0]}
+                      className="text-center"
+                      src={slide.image}
                       alt={`Slide ${index}`}
-                      style={{ width: "100%", height: "auto" }}
+                      style={{ width: "100%", height: "auto", backgroundColor: (slide.color != undefined ? slide.color : "") }}
                     />
                     <div className="slide-content">
-                      <h3>{slide.title}</h3>
-                      <p>{slide.description}</p>
+                      <h3 className="text-center py-4">{slide.title}</h3>
+                      {/* <p>{slide.description}</p> */}
                     </div>
                   </div>
                   </Link>
